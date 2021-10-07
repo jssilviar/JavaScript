785cff9b0a5f79deee6a2770e342ba8b5a3ff958
Silvia Jesus <jssilvia@gmail.com>
2021-10-07 16:25:56 -0500

    desafio clase4

--- CLASE4JS/funcion.js
+++ CLASE4JS/funcion.js
@@ -1,78 +1,79 @@
 //FUNCIONES
 //sin parámetros
-// function restar() {
-//     let numero1=parseInt(prompt("Ingrese el 1er número"));
-//     let numero2=parseInt(prompt("Ingrese el 2do número"));
-//     let resta=numero1-numero2;
-//     console.log("El resultado es: "+resta);
-// }
+function restar() {
+    let numero1=parseInt(prompt("Ingrese el 1er número"));
+    let numero2=parseInt(prompt("Ingrese el 2do número"));
+    let resta=numero1-numero2;
+    console.log("El resultado es: "+resta);
+}
 
-// restar();
+restar();
 
 
 // //CON PARÁMETROS
-// let resultado;
-// function restarConParametros(num1,num2){
-//     resultado=num1-num2;
-// }
+let resultado;
+function restarConParametros(num1,num2){
+    resultado=num1-num2;
+}
 
-// restarConParametros(89,9);
-// console.log("El resultado es: "+resultado);
+restarConParametros(89,9);
+console.log("El resultado es: "+resultado);
 
 //EJEM 1 PARA EDADES
-// let edad1=parseInt(prompt("Ingrese edad de la 1era persona"));
-// let edad2=parseInt(prompt("Ingrese edad de la 2da persona"));
-// restarConParametros(edad1,edad2);
-// console.log("la diferencia de edad entre las personas es: " +resultado);
+let edad1=parseInt(prompt("Ingrese edad de la 1era persona"));
+let edad2=parseInt(prompt("Ingrese edad de la 2da persona"));
+restarConParametros(edad1,edad2);
+console.log("la diferencia de edad entre las personas es: " +resultado);
 
 //EJEM 2 PARA AÑO DE NACIMIENTO
-// let anioDeNacimiento=parseInt(prompt("Ingrese su año de nacimiento:"));
-// const anioActual=2021;
-// restarConParametros(anioActual,anioDeNacimiento);
-// console.log("La edad que tienes es:  " +resultado + "años");
+let anioDeNacimiento=parseInt(prompt("Ingrese su año de nacimiento:"));
+const anioActual=2021;
+restarConParametros(anioActual,anioDeNacimiento);
+console.log("La edad que tienes es:  " +resultado + "años");
 
 //CON PARAMETROS Y RETONRO
-// function concatenarTextos(texto1, texto2){
-// let textoConcatenado = texto1 + " " + texto2;
-// return textoConcatenado; //Mariano Vasquez
-// }
+function concatenarTextos(texto1, texto2){
+let textoConcatenado = texto1 + " " + texto2;
+return textoConcatenado; //Mariano Vasquez
+}
 
-// let nombre=prompt("Ingresar nombre:");
-// let apellido=prompt("Ingresar apellido:");
-// let nombreCompleto= concatenarTextos(nombre, apellido);
-// console.log("buenos dias " + nombreCompleto);
+let nombre=prompt("Ingresar nombre:");
+let apellido=prompt("Ingresar apellido:");
+let nombreCompleto= concatenarTextos(nombre, apellido);
+console.log("buenos dias " + nombreCompleto);
 
 //DIAPO 
-// function calculadora(primerNumero, segundoNumero, operacion) { //EL ORDEN DE LOS PARAMETROS DECLARADOS NO INFLUYE EN EL RESULTADO, SIEMPRE Y CUANDO EN EL CONSOLE.LOG SE DECLARE DE LA MISMA FORMA
-//     switch (operacion) {
-//         case "+":
-//             return primerNumero + segundoNumero;
-//             break;
-//         case "-":
-//             return primerNumero - segundoNumero;
-//             break;
-//         case "*":
-//             return primerNumero * segundoNumero;
-//             break;
-//         case "/":
-//             return primerNumero / segundoNumero;
-//             break;
-//         default:
-//             return 0;
-//             break;
-//     }
-// }
-// console.log(calculadora(10, 5, "*"));
+function calculadora(primerNumero, segundoNumero, operacion){
+//EL ORDEN DE LOS PARAMETROS DECLARADOS NO INFLUYE EN EL RESULTADO, SIEMPRE Y CUANDO EN EL CONSOLE.LOG SE DECLARE DE LA MISMA FORMA
+    switch (operacion) {
+        case "+":
+            return primerNumero + segundoNumero;
+            break;
+        case "-":
+            return primerNumero - segundoNumero;
+            break;
+        case "*":
+            return primerNumero * segundoNumero;
+            break;
+        case "/":
+            return primerNumero / segundoNumero;
+            break;
+        default:
+            return 0;
+            break;
+    }
+}
+console.log(calculadora(10, 5, "*"));
 
-// let valor1=parseFloat(prompt("ingrese un numero:"));
-// let valor2=parseFloat(prompt("ingrese otro numero:"));
-// let ope=prompt("ingrese la operacion a realizar + - * /");
-// let resultadoFinal=calculadora(valor1, ope, valor2);
-// alert("El resultado de " + valor1 +" "+ ope + " " + valor2 + " = " + resultadoFinal);
+let valor1=parseFloat(prompt("ingrese un numero:"));
+let valor2=parseFloat(prompt("ingrese otro numero:"));
+let ope=prompt("ingrese la operacion a realizar + - * /");
+let resultadoFinal=calculadora(valor1, ope, valor2);
+alert("El resultado de " + valor1 +" "+ ope + " " + valor2 + " = " + resultadoFinal);
 
 //FUNCIONES FLECHA
 //sin parámetros
-/* let saludo=()=>{
+let saludo=()=>{
     let miNombre=prompt("Ingresa tu nombre");
     alert("Espero que estes bien " + miNombre);
 }
@@ -91,7 +92,7 @@ let nomb=prompt("Ingresa tu nombre");
 saludo2(nomb);
 
 //ejem3
-saludo2(prompt("quien eres?"));  */
+saludo2(prompt("quien eres?")); 
 
 const suma  = (a,b) => a + b;
 const resta = (a,b) => a - b;
