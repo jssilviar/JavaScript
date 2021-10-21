90fcff33ed1e9f04762361f093b6327a1cded88d
Silvia Jesus <jssilvia@gmail.com>
2021-10-21 16:02:07 -0500

    desafio #8

--- CLASE8JS/dom.js
+++ CLASE8JS/dom.js
@@ -34,9 +34,9 @@ function activarDark(){
 
 //CAMPOS
 document.getElementById("empresa").value="Mercado Libre";
-const productos = [{ id: 1,  nombre: "Arroz", precio: 125 },
+const productos = [{ id: 1,  nombre: "Arroz", precio: 125},
                   {  id: 2,  nombre: "Fideo", precio: 70 },
-                  {  id: 3,  nombre: "Pan"  , precio: 50},
+                  {  id: 3,  nombre: "Pan"  , precio: 50 },
                   {  id: 4,  nombre: "Flan" , precio: 100}
                 ];
 document.getElementById("cantidad").value= productos.length;
