345f2b4cacd8f20db7bdf5e17801eb83f5b6e8ae
Silvia Jesus <jssilvia@gmail.com>
2021-10-28 17:02:29 -0500

    creando tabla de datos cliente

--- desafioClase8/desafioClase6/desafioClase5/desafioClase4/eventos.js
+++ desafioClase8/desafioClase6/desafioClase5/desafioClase4/eventos.js
@@ -25,7 +25,7 @@ function saludo () {
     etiquetaNombre.style.textAlign = "center";
 }
 
-saludo();
+//saludo();
 
 const productos =[{ nombre:"Catering", precio :100 },
                   { nombre:"Cakes", precio:80 },
@@ -33,9 +33,9 @@ const productos =[{ nombre:"Catering", precio :100 },
 
 let botonProductos = document.getElementById("productos");
 
-botonProductos.addEventListener("click", lista);
+botonProductos.addEventListener("click", imprimeTabla);
 
-function lista(){
+function imprimeTabla(){
     let tabla = document.createElement("table");
     tabla.setAttribute("class","table table-success table-striped");
 
@@ -65,7 +65,59 @@ botonReset.addEventListener("click", () => {
     pedido.value = 0;
 });
 
+function Categorias(personas,keke,personaje,anios, presupuesto){
+    this.personas = personas;
+    this.keke = keke;
+    this.personaje = personaje;
+    this.anios = anios;
+    this.presupuesto = presupuesto;
+}
 
+let inputNombre = document.getElementById("inputNombre");
+let inputNombre1 = document.getElementById("inputNombre1");
+let inputNombre2 = document.getElementById("inputNombre2");
+let inputNombre3 = document.getElementById("inputNombre3");
+function datoCliente(event){
+    event.preventDefault();
+    if(inputNombre.value === "" || inputNombre1.value === ""|| inputNombre2.value === "" || inputNombre3.value === "")
+    {
+        alert("Por favor rellene todos los campos del formulario")
+    } else {
+        console.log(inputNombre.value);
+        console.log(inputNombre1.value);
+        console.log(inputNombre2.value);
+        console.log(inputNombre3.value);
+        
+        let tablapedidos = document.createElement("table");
+        tablapedidos.setAttribute("class","table table-success table-striped");
+    
+        let tablaHead = document.createElement("thead");
+        tablaHead.innerHTML = `<thead>
+                                    <tr>
+                                        <th scope="col">Modelo de torta</th>
+                                        <th scope="col">N° personas</th>
+                                        <th scope="col">Temática</th>
+                                        <th scope="col">Tipo de celebración</th>
+                                    </tr>
+                                </thead>`;
+        let tablaBody = document.createElement("tbody");
+        let fila = document.createElement("tr");
+        fila.innerHTML = `<td> ${inputNombre.value}</td>
+            <td><b>${inputNombre1.value}</b></td><td><b>${inputNombre2.value}</b></td><td><b>${inputNombre3.value}</b></td>`;
+        tablaBody.appendChild(fila);
+
+        tablapedidos.appendChild(tablaHead);
+        tablapedidos.appendChild(tablaBody);
+
+        document.getElementById("inferior2").appendChild(tablapedidos);
+        inputNombre.value = "";
+        inputNombre1.value = "";
+        inputNombre2.value = "";
+        inputNombre3.value = "";
+        
+    }
+   
+}
 
 
 
