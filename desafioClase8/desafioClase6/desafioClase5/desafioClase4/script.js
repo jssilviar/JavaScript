fa12d21ad67da0db88b9100f7e11b7ad2b7bc0c6
Silvia Jesus <jssilvia@gmail.com>
2021-11-05 17:53:55 -0500

    incorporando JQuery a mi proyecto

--- desafioClase8/desafioClase6/desafioClase5/desafioClase4/script.js
+++ desafioClase8/desafioClase6/desafioClase5/desafioClase4/script.js
@@ -8,9 +8,11 @@ for (const producto of productos) {
             <h5 id="nombre-producto-${producto.id}" class="card-title font--big">${producto.nombre}</h5>
             <p class="card-text font--large">${producto.desc}</p>
         </div>
-        <ul class="list-group list-group-flush">
-            <li class="list-group-item font--medium">S/ <span id="precio-producto-${producto.id}">${producto.precio}</span></li>
-        </ul>
+        <div class="cardPrecio">
+            <ul class="list-group list-group-flush"><strong>
+                <li class="list-group-item font--medium">S/ <span id="precio-producto-${producto.id}">${producto.precio}</span></li>
+            </ul>
+        </div>
         <div class="cantidad font--small">
             <button id="${producto.id}">Añadir</button>
             <button id="reset-${producto.id}" class="card-link" onclick="borre()">Borrar</button>
@@ -20,17 +22,24 @@ for (const producto of productos) {
     gridCarro.appendChild(cardProducto);
 
     document.getElementById(`${producto.id}`).onclick = () => agregarAlCarrito(`${producto.id}`);
+    $(".cardCarrito").css({ background: 'darkkhaki', color: 'black' });
+    $(".cardPrecio").css({ background: '#f5db5d', color: 'black' });
 }
+
+$("#vaciarCarrito").prepend("<button id='btnVaciarCarrito' class='btn btn-warning' onclick='alertar();' disabled>Vaciar el carrito</button>");
 
 let carrito = [];
 
 let carritoSize = document.getElementById("cantidadProductos");
 let detalleProductos = document.getElementById(`detalleProductos`);
+let btnVaciarCarrito = document.getElementById(`btnVaciarCarrito`);
 
 function agregarAlCarrito(id) {
     let precioProducto = document.getElementById(`precio-producto-${id}`).textContent;
     let productoNombre = document.getElementById(`nombre-producto-${id}`).textContent;
-    if (carrito.length === 0) {
+    let carritoString = localStorage.getItem("carrito");
+    if (carritoString == null) {
+        btnVaciarCarrito.disabled = false;
         carrito.push({
             idProducto: id,
             productoNombre: productoNombre,
@@ -77,12 +86,39 @@ function actualizarCarrito() {
 }
 
 function actualizarDetalle() {
-    detalleProductos.innerHTML = "";
     let carritoString = localStorage.getItem("carrito");
-    let carrito = JSON.parse(carritoString);
-    for (let producto of carrito) {
-        let linea = document.createElement("pre");
-        linea.innerHTML = `<li>${producto.productoNombre} (${producto.unidades})        S/${Math.round(producto.precioProducto * producto.unidades)}.00</li>`;
-        detalleProductos.appendChild(linea);
+    if (carritoString === null) {
+        detalleProductos.innerHTML = "";
+    } else {
+        detalleProductos.innerHTML = "";
+        let carrito = JSON.parse(carritoString);
+        for (let producto of carrito) {
+            let linea = document.createElement("pre");
+            linea.innerHTML = `<li>${producto.productoNombre} (${producto.unidades})        S/${Number(producto.precioProducto * producto.unidades).toFixed(2)}</li>`;
+            detalleProductos.appendChild(linea);
+        }
     }
+}
+
+const alertar = () => {
+    let fecha = new Date().toLocaleDateString();
+    Swal.fire({
+        title: '¿Seguro que deseas eliminar todo los productos de tu bolsa de compra?',
+        showDenyButton: true,
+        showCancelButton: false,
+        confirmButtonText: 'Sí',
+        denyButtonText: `No`,
+        icon: 'warning',
+    }).then((result) => {
+        if (result.isConfirmed) {
+            Swal.fire('Eliminado!', '', 'success');
+            localStorage.removeItem("carrito");
+            actualizarDetalle();
+            carrito.length = 0;
+            carritoSize.textContent = 0;
+            btnVaciarCarrito.disabled = true;
+        } else if (result.isDenied) {
+            Swal.fire('No se ha eliminado los productos', '', 'info')
+        }
+    })
 }
