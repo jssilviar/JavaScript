84535f20e32cdfe8db9ef98a3977745acf51f7cc
Silvia Jesús <jssilviar@gmail.com>
2021-12-03 01:29:48 -0500

    Proyecto finalizado

--- script.js
+++ script.js
@@ -1,333 +1,207 @@
-/* let gridCarro = document.getElementById("gridCarro");
-//llamar json de productos
-function cargarProductos() {
-    fetch('./productos.json')
-        .then(data => console.log(data.json()))
-        // .then(p)
+//Declarar las etiquetas en las que se cargarán los productos del archivo json
+const backing1 = document.getElementById("backing1");
+const backing2 = document.getElementById("backing2");
+const cakes1 = document.getElementById("cakes1");
+const cakes2 = document.getElementById("cakes2");
+const catering1 = document.getElementById("catering1");
+const catering2 = document.getElementById("catering2");
+
+const btnFinalizarCompra = document.getElementById("finalizarCompra");
+
+//Declarar los arrays
+let productos;
+let palletProducts = [];
+let cakesProducts = [];
+let cateringProducts = [];
+let arrayCarrito = [];
+
+async function cargarProductos() {
+    productos = await fetch('./productos.json')
+                            .then(res => res.json())
+                            .then(data => data.productos);
+    palletProducts = filtrarProductosPorCategoria(productos, "backingPallet");            
+    cakesProducts = filtrarProductosPorCategoria(productos, "cakes");
+    cateringProducts = filtrarProductosPorCategoria(productos, "catering");
+    pintarArray(backing1, palletProducts.slice(0,3));
+    pintarArray(backing2, palletProducts.slice(3,6));
+    pintarArray(cakes1, cakesProducts.slice(0, 3));
+    pintarArray(cakes2, cakesProducts.slice(3, 6));
+    pintarArray(catering1, cateringProducts.slice(0, 3));
+    pintarArray(catering2, cateringProducts.slice(3, 6));
+    localStorage.setItem("productos", JSON.stringify(productos));      
 }
  
-cargarProductos()*/
+cargarProductos();
 
-
-let contenido = document.querySelector('#resumen');
-
-function anadir (){
-    fetch('productos.json')
-        .then(res => res.json() )
-        .then( datos => {
-            //console.log(datos)
-            tabla(datos)
-    } )
-    
+function filtrarProductosPorCategoria(arreglo, categoria) {
+    return arreglo.filter(item => item.categoria == categoria);
 }
 
-function tabla(datos){
-    console.log(datos)
-    resumen.innerHTML = ''
-    for(let valor of datos){
-        console.log(valor)
-        /* resumen.innerHTML += `
-
-        <tr>
-            <th scope="row">${valor.nombre}</th>
-            <th>${valor.imagen}</th>
-            <th>${valor.precio}</th>
-            <th>${valor.cantidad}</th>
-        </tr>
-        ` */
+//Pintar cards
+function pintarArray(contenedor, arreglo) {
+    for(producto of arreglo) {
+        let item = document.createElement("div");
+        item.setAttribute("class", "col-sm-12 col-lg-4");
+        item.innerHTML =   `<div class="card">
+                                <img src="img/${producto.imagen}" class="card-img-top redondeado" alt="pallet blanco">
+                            </div>
+                            <div class="card-body">
+                                <h3 class="card-title font--big">${producto.nombre}</h3>
+                                <p class="card-text font--large">${producto.desc}</p>
+                                <ul class="list-group list-group-flush">
+                                    <li class="list-group-item font--medium">Desde S/${Number(producto.precio).toFixed(2)}</li>
+                                </ul>
+                                <button type="button" class="btn btn-warning" onclick="anadirCarrito(${producto.id})">Añadir al Carrito</button>
+                            </div>`;
+        contenedor.appendChild(item);
     }
-    
-    
-    
-    
-
 }
 
-
-
-
-
-/* //cambiando de color en la nav
-const backingPallet = document.querySelector('#backingPallet');
-const cakes = document.querySelector('#cakes');
-const catering = document.querySelector('#catering');
-const contactanos = document.querySelector('#contactanos');
-const ubicanos = document.querySelector('#ubicanos');
-const carritoDeCompras = document.querySelector('#carritoDeCompras');
-
-const itemBackingPallet = document.querySelector('#li-backingPallet');
-const itemCakes = document.querySelector('#li-cakes');
-const itemCatering = document.querySelector('#li-catering');
-const itemContactanos = document.querySelector('#li-contactanos');
-const itemUbicanos = document.querySelector('#li-ubicanos');
-const itemCarritoDeCompras = document.querySelector('#li-carritoDeCompras');
-
-const ob1 = new IntersectionObserver(callback);
-
-function callback(entries, observador) {
-    if (entries[0].isIntersecting) {
-        itemBackingPallet.classList.add('header__sombra');
+//Añadir producto
+function anadirCarrito(productoId){
+    let {id, nombre, imagen, precio} = buscarProductoPorId(productoId);
+    let producto = {
+        id, nombre, imagen, precio, unidades: 1
+    }
+    if(arrayCarrito.length == 0){
+        arrayCarrito.push(producto);
+        Swal.fire({
+            position: 'center',
+            icon: 'success',
+            title: 'Se añadió un producto nuevo al carrito',
+            showConfirmButton: false,
+            timer: 2000
+          })
+        btnFinalizarCompra.disabled = false;
     } else {
-        itemBackingPallet.classList.remove('header__sombra');
+        let existeEnCarrito = arrayCarrito.find( ({id}) => id === producto.id);
+        if (existeEnCarrito === undefined) {
+            arrayCarrito.push(producto);
+            Swal.fire({
+                position: 'center',
+                icon: 'success',
+                title: 'Se añadió un producto nuevo al carrito',
+                showConfirmButton: false,
+                timer: 2000
+              })
+        } else {
+            existeEnCarrito.unidades += 1;
+        }
     }
-}
-
-ob1.observe(backingPallet);
-
-const ob2 = new IntersectionObserver(callback2);
-const ob3 = new IntersectionObserver(callback3);
-
-ob2.observe(cakes);
-ob3.observe(catering);
-
-function callback2(entries, observador) {
-    if (entries[0].isIntersecting) {
-        itemBackingPallet.classList.remove('header__sombra');
-        itemCakes.classList.add('header__sombra');
-    } else {
-        itemCakes.classList.remove('header__sombra');
-    }
-}
-
-function callback3(entries, observador) {
-    if (entries[0].isIntersecting) {
-        itemCakes.classList.remove('header__sombra');
-        itemCatering.classList.add('header__sombra');
-    } else {
-        itemCatering.classList.remove('header__sombra');
-    }
-}
-
-if (localStorage.getItem("carrito")) {
-    localStorage.removeItem("carrito");
-}
-
-/* 
-
-for (const producto of productos) {
-    let cardProducto = document.createElement("div");
-    cardProducto.setAttribute("class", "cardCarrito");
-    cardProducto.innerHTML = `
-        <img src="./imagenes/${producto.imagen}" class="card-img-top" alt="${producto.imagen}">
-        <div class="card-body">
-            <h5 id="nombre-producto-${producto.id}" class="card-title font--big">${producto.nombre}</h5>
-            <p class="card-text font--large">${producto.desc}</p>
-        </div>
-        <div class="cardPrecio">
-            <ul class="list-group list-group-flush"><strong>
-                <li class="list-group-item font--medium">S/ <span id="precio-producto-${producto.id}">${producto.precio}</span></li>
-            </ul>
-        </div>
-        <div class="cantidad font--small">
-            <button id="${producto.id}">Añadir</button>
-            <button id="reset-${producto.id}" class="card-link" onclick="borre()">Borrar</button>
-        </div>
-    <p style="display: none">${producto.id}</p>`;
-
-    gridCarro.appendChild(cardProducto);
-
-    document.getElementById(`${producto.id}`).onclick = () => agregarAlCarrito(`${producto.id}`);
-    $(".cardCarrito").css({ background: 'darkkhaki', color: 'black' });
-    $(".cardPrecio").css({ background: '#f5db5d', color: 'black' });
-}
-
-$("#vaciarCarrito").prepend("<button id='btnVaciarCarrito' class='btn btn-warning' onclick='alertar();' disabled>Vaciar el carrito</button>");
-
-let carrito = [];
-
-let carritoSize = document.getElementById("cantidadProductos");
-let detalleProductos = document.getElementById(`detalleProductos`);
-let btnVaciarCarrito = document.getElementById(`btnVaciarCarrito`);
-
-$("#mostrarOcultar").hide();
-
-function agregarAlCarrito(id) {
-    let precioProducto = parseFloat(document.getElementById(`precio-producto-${id}`).textContent);
-    let productoNombre = document.getElementById(`nombre-producto-${id}`).textContent;
-    let carritoString = localStorage.getItem("carrito");
-    if (carritoString == null) {
-        btnVaciarCarrito.disabled = false;
-        carrito.push({
-            idProducto: id,
-            productoNombre: productoNombre,
-            unidades: 1,
-            precioProducto: precioProducto
-        });
-        localStorage.setItem("carrito", JSON.stringify(carrito));
-        $("#mostrarOcultar").show();
-
-    } else {
-        let carritoGuardado = localStorage.getItem("carrito");
-        //Recuperé el carrito en JSON
-        let carritoJSON = JSON.parse(carritoGuardado);
-
-        //buscar si dentro del array existe el id ingresado
-        existeId(carritoJSON, id, productoNombre, precioProducto);
-    }
+    localStorage.setItem("carrito", JSON.stringify(arrayCarrito));
     actualizarCarrito();
 }
 
-function existeId(arreglo, idIngresado, nombreIngresado, precioIngresado) {
-    let producto = arreglo.find(item => item.idProducto === idIngresado);
-    if (producto === undefined) {
-        arreglo.push({
-            idProducto: idIngresado,
-            productoNombre: nombreIngresado,
-            unidades: 1,
-            precioProducto: precioIngresado
-        });
-        localStorage.setItem("carrito", JSON.stringify(arreglo));
-    } else {
-        producto.unidades += 1;
-        localStorage.setItem("carrito", JSON.stringify(arreglo));
-    }
+function buscarProductoPorId(id) {
+    let productosEnMemoria = localStorage.getItem("productos");
+    let productosJSON = JSON.parse(productosEnMemoria);
+    return productosJSON.find(item => item.id === id);
 }
 
+const conteo = document.getElementById("cantidadProductos");
+const totalCompra = document.getElementById("totalCompra");
 function actualizarCarrito() {
     let carritoString = localStorage.getItem("carrito");
     let carrito = JSON.parse(carritoString);
     let unidades = 0;
+    let totalCarrito = 0;
     for (let item of carrito) {
-        unidades += item.unidades
+        unidades += item.unidades;
+        let totalPorItem = item.unidades * item.precio;
+        totalCarrito += totalPorItem;
+    } if (unidades === 0){
+        btnFinalizarCompra.disabled = true;
     }
-    carritoSize.textContent = unidades;
-    actualizarDetalle();
+    totalCompra.textContent = Number(totalCarrito).toFixed(2);
+    conteo.textContent = unidades;
+    actualizarTabla(carrito);
 }
 
-function actualizarDetalle() {
-    let carritoString = localStorage.getItem("carrito");
-    if (carritoString === null) {
-        detalleProductos.innerHTML = "";
+//Crear tabla
+const tablaDetalle = document.getElementById("resumen");
+function actualizarTabla(arreglo) {
+    if (arreglo === null) {
+        tablaDetalle.innerHTML = "";
     } else {
-        detalleProductos.innerHTML = "";
-        let carrito = JSON.parse(carritoString);
-        let totalItem
-        let totalCompra
-        for (let producto of carrito) {
+        tablaDetalle.innerHTML = "";
+        for (let productoSeleccionado of arreglo) {
             let linea = document.createElement("tr");
-            linea.innerHTML = `<td>${producto.productoNombre} (${producto.unidades})</td><td> S/${Number(producto.precioProducto * producto.unidades).toFixed(2)}<button id="btn-delete-${producto.idProducto}" onclick="deleteProduct(${producto.idProducto})">Eliminar</button></td>`;
-            detalleProductos.appendChild(linea);
-            totalCompra = +(producto.precioProducto * producto.unidades);
-            console.log(totalCompra);
+            linea.innerHTML = `<td>${productoSeleccionado.nombre}</td>
+                                <td><img src="./img/${productoSeleccionado.imagen}" class="imgResumen redondeado"></td>
+                                <td>S/${Number(productoSeleccionado.precio).toFixed(2)}</td>
+                                <td><input type="number" class="unidades" min="1" max="10" value="${productoSeleccionado.unidades}" readonly/></td>
+                                <td><i class="fas fa-trash-alt delete" onclick="deleteProduct(${productoSeleccionado.id})"></i></td>`;
+            tablaDetalle.appendChild(linea);
         }
     }
 }
 
-/*function deleteProduct(id) {
-    console.log("Eliminando el producto:", id);
-    let carritoString = localStorage.getItem("carrito");
-    let carrito = JSON.parse(carritoString);
-    carrito.find(item => {
-        if (id === item.idProducto) {
-
-        }
-    })
-}
-
- 
-$(`#eliminar${productoNombre}`).click(function()){
-    let eliminarProd
-} 
-
-const alertar = () => {
-    let fecha = new Date().toLocaleDateString();
+//Borrar producto
+function deleteProduct(id){
     Swal.fire({
-        title: '¿Seguro que deseas eliminar todo los productos de tu bolsa de compra?',
-        showDenyButton: true,
-        showCancelButton: false,
-        confirmButtonText: 'Sí',
-        denyButtonText: `No`,
+        title: '¿Estás seguro?',
+        text: "Se eliminará este producto de tu carrito",
         icon: 'warning',
-    }).then((result) => {
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Sí, borrar!'
+      }).then((result) => {
         if (result.isConfirmed) {
-            Swal.fire('Eliminado!', '', 'success');
-            localStorage.removeItem("carrito");
-            actualizarDetalle();
-            carrito.length = 0;
-            carritoSize.textContent = 0;
-            btnVaciarCarrito.disabled = true;
-            $("#mostrarOcultar").hide();
-        } else if (result.isDenied) {
-            Swal.fire('No se ha eliminado los productos', '', 'info')
+            Swal.fire(
+                'Eliminado!',
+                'El producto ha sido eliminado',
+                'success'
+            );
+            let nuevoArreglo = arrayCarrito.filter( item => item.id !== id);
+            arrayCarrito = nuevoArreglo;
+            actualizarTabla(arrayCarrito);
+            localStorage.setItem("carrito", JSON.stringify(arrayCarrito));
+            actualizarCarrito();
         }
-    })
+      });     
 }
 
-//mostrarOcultar detalles del carro de compras
-$("#mostrarOcultar").click(function() {
-    if ($("#mostrarOcultar").html() == "Ocultar") {
-        $("#mostrarOcultar").html(" Mostrar");
-    } else {
-        $("#mostrarOcultar").html("Ocultar");
-    }
-    $("#detalleProductos").fadeToggle(1000);
-}); */
-
-//seccion contactanos
-const nombre = document.getElementById('nombre');
-const apeliidos = document.getElementById('apeliidos');
-const inputCity = document.getElementById('inputCity');
-const inputAddress = document.getElementById('inputAddress');
-const Email = document.getElementById('E-mail');
-const mensaje = document.getElementById('mensaje');
-const calendario = document.getElementById('calendario');
-const celular = document.getElementById('celular');
-error.style.color = '#ffc107';
-
-/* function enviarForm() {
-    if (nombre.value === null || nombre.value === '') {
-        mensajesError.push('Ingresa tu nombre');
-    }
-    if (apeliidos.value === null || apeliidos.value === '') {
-        mensajesError.push('Ingresa tus apellidos');
-    }
-    if (inputCity.value === null || inputCity.value === '') {
-        mensajesError.push('Ingresa tu dirección');
-    }
-    if (inputAddress.value === null || inputAddress.value === '') {
-        mensajesError.push('Indicanos alguna referencia');
-    }
-    if (Email.value === null || Email.value === '') {
-        mensajesError.push('Ingresa tu e-mail');
-    }
-    if (mensaje.value === null || mensaje.value === '') {
-        mensajesError.push('Dejanos un mensaje');
-    }
-    if (calendario.value === null || calendario.value === '') {
-        mensajesError.push('Ingresa la fecha aproximada de tu evento');
-    }
-    if (celular.value === null || celular.value === '') {
-        mensajesError.push('Indicanos tu N° de teléfono');
-    }
-
-    error.innerHTML = mensajesError.join(',')
-
-    retun: false
-} */
-const form = document.getElementById('formulario');
-form.addEventListener('submit', function(event) {
-    console.log("enviando formulario");
-
-})
-
-
-
-
-
-
-
-let enviar = document.getElementById("btnEnviar").onclick = () => {
-
-    Swal.fire({
-        position: 'top-end',
-        icon: 'success',
-        title: 'Tu mensaje a sido enviado, nos estaremos comunicando contigo. Gracias por tu preferencia.',
-        showConfirmButton: false,
-        timer: 1500,
-    })
+//Finalizar compra
+btnFinalizarCompra.addEventListener("click", handleFinCompra);
+function handleFinCompra(){
+    let total =  Number(totalCompra.textContent).toFixed(2)
+    Swal.fire(
+        `Tu compra ha sido por S/${total}`,
+        '¡Gracias por tu preferencia!',        
+        'success'
+    );
 }
 
-//sombreado de secciones
+//comprobando formulario
+const formulario = document.getElementById("formulario");
+const mandarCorreo = document.getElementById("mandarCorreo");
+formulario.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const data = new FormData(this);
+    let nombre = data.get('nombre');
+    let inputAddress = data.get('inputAddress');
+    let referencia = data.get('referencia');
+    let email = data.get('email');
+    let mensaje = data.get('mensaje');
+    let calendario = data.get('calendario');
+    let celular = data.get('celular');
+    let encabezado = "COTIZACIÓN"
+    let body = `CLIENTE: ${nombre}\
+    DIRECCIÓN: ${inputAddress}\
+    REFERENCIA: ${referencia}\
+    EMAIL: ${email}\
+    TELÉFONO: ${celular}\
+    FECHA DE EVENTO: ${calendario}\
+    MENSAJE: ${mensaje}`;
+    mandarCorreo.setAttribute('href', `mailto:jssilviar@gmail.com?subject=${encabezado}&body=${body}`);
+    mandarCorreo.click();
+    this.reset();
+}
+
+//sombreado de secciones de la nav
 const secciones = document.querySelectorAll('.seccion');
 const menuItems = document.querySelectorAll('.menu_item');
 
